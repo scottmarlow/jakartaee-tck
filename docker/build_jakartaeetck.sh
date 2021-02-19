1dbb36a131c75d2199f6be1b82eee7f7b1f400b7
Scott Marlow <smarlow@redhat.com>
2021-02-19 15:59:10 -0500

    hack on sigtest changes
    Signed-off-by: Scott Marlow <smarlow@redhat.com>

--- docker/build_jakartaeetck.sh
+++ docker/build_jakartaeetck.sh
@@ -97,6 +97,28 @@ mkdir -p $JAKARTA_JARS/endorsed
 
 mvn -f $BASEDIR/docker/pom.xml dependency:copy-dependencies -DoutputDirectory="${JAKARTA_JARS}/modules" -Dmdep.stripVersion=true
 
+echo "generate TCK signature map files"
+cd $JAKARTA_JARS/lib
+# replace existing (https://wiki.openjdk.java.net/display/CodeTools/sigtest) sigtest.jar with latest jar from (fork) https://github.com/jtulach/netbeans-apitest
+mvn dependency:get -Dartifact=org.netbeans.tools:sigtest-maven-plugin:LATEST:jar -Dtransitive=false -Ddest=sigtest.jar
+export sigtest_classes="${JAKARTA_JARS}/lib/sigtest.jar"
+export pathsep=:
+export deliverabledir=signature-repository
+export tckname=jakartaee
+export TS_HOME=$JAKARTA_JARS
+
+cd $JAKARTA_JARS/src/com/sun/ts/tests/signaturetest
+script /tmp/sigtest.log
+cd /home/smarlow/work/jakarta/jakartaee-tck/src/com/sun/ts/tests/signaturetest
+for tckjarname in $JAKARTA_JARS/modules/jakarta*.jar; do
+  [ -e "$tckjarname" ] || continue
+  # generate signature for each jakarta*.jar 
+  echo "generate signature for $tckjarname"
+  sigTestClasspath=${pathsep}${tckjarname}${pathsep}${JAVA_HOME}/jre/lib/rt.jar${pathsep}
+  ant -find record-build.xml -Dsig.source="${sigtest_classes}${pathsep}${sigTestClasspath}" -Dmap.file=${TS_HOME}/install/jakartaee/bin/sig-test_se8.map -Ddeliverabledir="modules"   -Drecorder.type=sigtest record.sig.batch 
+
+done
+
 #TODO : security jar is required to compile SunRILoginContext.java, look for alternative way to remove this dependency
 wget --progress=bar:force --no-cache https://repo1.maven.org/maven2/org/glassfish/security/security/3.1.1/security-3.1.1.jar -O ${JAKARTA_JARS}/modules/security.jar
 
