bed77bc99967c24e9f486256255d1b6fad885e36
Scott Marlow <scott.marlow@ibm.com>
2026-04-03 10:55:32 -0400

    Of these three fixes, one is to clear the extended persistence context that might still have previous test results and other two are to fix classloader portability issues that are for the tck issue already accepted.
    Signed-off-by: Scott Marlow <scott.marlow@ibm.com>

--- tcks/apis/persistence/persistence-inside-container/platform-tests/src/main/java/ee/jakarta/tck/persistence/ee/packaging/appclient/annotation/ClientTest.java
+++ tcks/apis/persistence/persistence-inside-container/platform-tests/src/main/java/ee/jakarta/tck/persistence/ee/packaging/appclient/annotation/ClientTest.java
@@ -53,9 +53,7 @@ public class ClientTest extends ee.jakarta.tck.persistence.ee.packaging.appclien
             ee.jakarta.tck.persistence.ee.packaging.appclient.annotation.Coffee.class,
             ee.jakarta.tck.persistence.ee.packaging.appclient.annotation.Client.class,
             EETest.class,
-            com.sun.ts.lib.util.TestUtil.class,
-            SetupException.class,
-            com.sun.ts.lib.harness.Status.class
+            SetupException.class
             );
             // The application-client.xml descriptor
 
